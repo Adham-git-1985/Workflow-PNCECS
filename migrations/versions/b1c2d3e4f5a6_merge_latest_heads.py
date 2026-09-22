3c0f09d7a83fbf56df6da8fd3e38788b47083ef9
Adham-git-1985 <adham.han@gmail.com>
2026-09-23 02:07:51 +0300

    Merge Alembic migration heads

---
+++ migrations/versions/b1c2d3e4f5a6_merge_latest_heads.py
@@ -0,0 +1,20 @@
+"""merge the active migration heads
+
+Revision ID: b1c2d3e4f5a6
+Revises: a4b5c6d7e8f9, z8a9b0c1d2e3, z9a0b1c2d3e4
+Create Date: 2026-09-23
+"""
+
+
+revision = "b1c2d3e4f5a6"
+down_revision = ("a4b5c6d7e8f9", "z8a9b0c1d2e3", "z9a0b1c2d3e4")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
